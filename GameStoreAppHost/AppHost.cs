76372a39f4bc5edbbde7db19ed1a6f414441f1fa
Joe Chandler <joseph-chandler@sky.com>
2025-11-24 18:28:36 +0000

    Added Aspire tooling

---
+++ GameStoreAppHost/AppHost.cs
@@ -0,0 +1,20 @@
+using Projects;
+
+var builder = DistributedApplication.CreateBuilder(args);
+
+var postgres = builder.AddPostgres("postgres")
+                      .WithDataVolume()
+                      .WithPgAdmin(pgAdmin => pgAdmin.WithHostPort(5050));
+
+var database = postgres.AddDatabase("GameStore");
+
+var redis = builder.AddRedis("redis");
+
+builder.AddProject<GameStoreApi>("gamestore-api")
+       .WithHttpHealthCheck("/health")
+       .WithReference(database)
+       .WaitFor(database)
+       .WithReference(redis)
+       .WaitFor(redis);
+
+builder.Build().Run();
